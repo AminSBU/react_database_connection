2f8ac82083a86420658e867d10cf90fd63e9e835
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-12-04 23:31:32 +0330

    connected to the database

--- react_database_connection/server.js
+++ react_database_connection/server.js
@@ -20,7 +20,7 @@ const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'react',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || '1234',
   port: process.env.DB_PORT || '',
 });
 
